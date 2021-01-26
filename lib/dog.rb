632646c9a20eba5a173da0c3f01a053119cabb65
Austin Hernandez <ahernandez1170@gmail.com>
2021-01-26 23:01:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,16 @@
 class Dog
-  def name= (dogs_name)
+ 
+  def name=(dogs_name)
     @this_dogs_name = dogs_name
-    end
-    
-class Dog
-  def name 
+  end
+ 
+  def name
     @this_dogs_name
   end
 end
-  
-  french = Dog.new
-  french.name = "french"
-  
-  puts french.name
-  
-  french.bark = "Woof!"
-  
-  puts french.bark
+ 
+lassie = Dog.new
+lassie.name = "Lassie"
+ 
+puts lassie.name
+ 
